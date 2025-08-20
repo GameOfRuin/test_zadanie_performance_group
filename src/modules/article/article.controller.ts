ece3d4f910b384cff7ac2108cfdf95f04b27a157
Anatoly Kochetov <tolik.kochetov44@gmail.com>
2025-08-20 14:41:19 +0300

    feat: Добавлен поиск по номеру задачи

--- src/modules/article/article.controller.ts
+++ src/modules/article/article.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiCreatedResponse, ApiOperation } from '@nestjs/swagger';
 import { FastifyRequest } from 'fastify';
 import { JwtGuard } from '../../guards/jwt.guard';
+import { IdNumberDto } from '../../shared';
 import { ArticleService } from './article.service';
 import { ArticleResponseDto, CreateArticleDto } from './dto';
 
@@ -16,5 +17,12 @@ export class ArticleController {
   @Post('create')
   async creatArticle(@Body() dto: CreateArticleDto, @Req() request: FastifyRequest) {
     return this.articleService.creatArticle(dto, request.user);
+  }
+
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Поиск задачи по id' })
+  @Get('/:id')
+  async getArticle(@Param() { id }: IdNumberDto) {
+    return await this.articleService.getArticleById(id);
   }
 }
--- src/modules/article/article.service.ts
+++ src/modules/article/article.service.ts
@@ -1,8 +1,10 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { inject } from 'inversify';
+import { Includeable } from 'sequelize';
 import { redisArticleKey } from '../../cache/cache.keys';
 import { CacheService } from '../../cache/cache.servise';
 import { ArticlesEntity, UserEntity } from '../../database/entities';
+import { TimeInSeconds } from '../../shared';
 import { CreateArticleDto } from './dto';
 
 @Injectable()
@@ -14,6 +16,14 @@ export class ArticleService {
     private readonly redis: CacheService,
   ) {}
 
+  private readonly joinAuthor: Includeable[] = [
+    {
+      model: UserEntity,
+      as: 'author',
+      attributes: ['id', 'name'],
+    },
+  ];
+
   async creatArticle(dto: CreateArticleDto, user: UserEntity) {
     this.logger.log('Creating a new article');
 
@@ -22,5 +32,29 @@ export class ArticleService {
     await this.redis.set(redisArticleKey(newArticle.id), { newArticle }, { EX: 600 });
 
     return newArticle;
+  }
+  async getArticleById(idArticle: ArticlesEntity['id']) {
+    this.logger.log(`Чтение задачи по id=${idArticle}`);
+
+    const cacheTask = await this.redis.get<ArticlesEntity>(redisArticleKey(idArticle));
+
+    if (cacheTask) {
+      return cacheTask;
+    }
+
+    const task = await ArticlesEntity.findOne({
+      where: { id: idArticle },
+      include: [...this.joinAuthor],
+    });
+
+    if (!task) {
+      throw new NotFoundException('Такой задачи не найдено');
+    }
+
+    await this.redis.set(redisArticleKey(idArticle), task, {
+      EX: 10 * TimeInSeconds.minute,
+    });
+
+    return task.toJSON();
   }
 }
